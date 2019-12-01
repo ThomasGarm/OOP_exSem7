fa5c3d47bd61aeb83ab1ae81c4568b916012ddcb
python <thomas.win@gmail.com>
2019-12-01 12:15:58 +0100

    searching

--- ex3/book.py
+++ ex3/book.py
@@ -5,3 +5,6 @@ class Book():
     def __init__(self, title,pages):
         self.title = title
         self.pages = pages
+        for i in list:
+            if hasattr(self, i):
+                setattr(self, i)
--- ex3/library.py
+++ ex3/library.py
@@ -1,16 +1,32 @@
-# coding: utf8
+
+
+
 from book import *
+
 from books_list import *
 
 class Library():
-    """"""
-    def __init__(self):
+    
+    def __init__(self, books_list):
+        self.title = None
+        self.pages = None
+        for i in books_list:
+            if hasattr(self, i):
+                setattr(self, i)
+        
+
         
 
     def add_book(self, book):
-        """"""
-        pass
+        book = books_list
+        for i in book:
+            title = i[0]
+        for j in i:
+            pages = j[0::]
+        return title, pages
+
+        
 
     def get_book(self, title):
-        """"""
-       	pass
+        title = input("Choose your book")
+        return title
